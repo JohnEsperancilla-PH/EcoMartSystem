788e4282a0e458dc1f041a3044238be082ed730e
JohnEsperancilla-PH <109806312+JohnEsperancilla-PH@users.noreply.github.com>
2025-02-10 10:01:09 +0800

    feat: update styles and routing; import Open Sans font, apply Inter font to components, and change navbar link to index.php

--- routes.php
+++ routes.php
@@ -6,34 +6,4 @@ $router->add('GET', '/login', 'AuthController@login');
 $router->add('POST', '/login', 'AuthController@login');
 
 $page = isset($_GET['page']) ? $_GET['page'] : 'signup';
-
-switch ($page) {
-    case 'login':
-        include '../views/login.view.php';
-        break;
-    case 'setup':
-        include '../views/setup.view.php';
-        break;
-    case 'home':
-        include '../views/home.view.php';
-        break;
-    case 'shop':
-        include '../views/shop.view.php';
-        break;
-    case 'branches':
-        include '../views/branches.view.php';
-        break;
-    case 'contact':
-        include '../views/contact.view.php';
-        break;
-    case 'privacy-policy':
-        include '../views/privacy-policy.view.php';
-        break;
-    case 'process-order':
-        include '../views/process-order.view.php';
-        break;
-    default:
-        include '../views/signup.view.php';
-        break;
-}
 ?>
